9646d851697e2c7c6a50affd6d1f202d4cfabdf1
Melchalk <melm2004@mail.ru>
2025-04-21 18:32:17 +0300

    done lab 9

---
+++ Lab 9/PythonWeb/main.py
@@ -0,0 +1,17 @@
+from starlette.responses import FileResponse
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@app.get("/hello")
+def show_add_posts():
+    return FileResponse("hello_page.html")
